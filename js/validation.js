385c3294c668e4b727ed555c72b41d9fc22ddeab
Raulet Olha <raulet.olha@gmail.com>
2022-12-12 15:48:19 +0100

    Modify on change behaviour; modify tooltip creation messages

--- js/validation.js
+++ js/validation.js
@@ -1,28 +1,74 @@
 const form = document.querySelector("form");
 
-for (let element of form.elements) {
-    const type = element.type;
-    if (type != "submit") {
+validation(form);
+submit(form);
+
+
+function validation(form) {
+    for (const element of form.elements) {
         const elHelpText = document.getElementById(`${element.name}Help`);
-        element.addEventListener("invalid", (event) => {
-            event.preventDefault();
-            element.classList.add("is-invalid");
-            // const elHelpText = document.getElementById(`${element.name}Help`);
-            elHelpText.classList.add("text-danger");
-            const firstInvalidField = form.querySelector(":invalid");
-            firstInvalidField.focus();
-
-            tooltipMessage(element);
-
-            if (element.name == firstInvalidField.name) {
-                tooltip(firstInvalidField);
-            }
-        });
-        onChangeSuccess(element, elHelpText);
+        const type = element.type;
+        if (type != "submit") {
+            const opt = {
+                placement: "top",
+                trigger: "focus",
+                title: "Ce champ est obligatoire."
+            };
+            const tooltip = bootstrap.Tooltip.getOrCreateInstance(element, opt);
+            tooltip.disable();
+            onChange(element, elHelpText, tooltip);
+            invalid(element, elHelpText, tooltip)
+        }
     }
 }
 
-submit(form);
+//Change behaviour on ':invalid' fields
+function invalid(element, elHelpText, tooltip) {
+    element.addEventListener("invalid", (event) => {
+        event.preventDefault();
+        element.classList.add("is-invalid");
+        elHelpText.classList.add("text-danger");
+        const firstInvalidField = form.querySelector('.is-invalid');
+        element.setAttribute("data-bs-toggle", "tooltip");
+        firstInvalidField.focus();
+        tooltip.enable();
+    });
+}
+
+// Add message to tooltip "error validation"
+function tooltipMessage(element) {
+    let tooltip = bootstrap.Tooltip.getInstance(element);
+    if (element.name == "rate" && element.validity.valueMissing) {
+        tooltip.setContent({ '.tooltip-inner': 'Ce champ est obligatoire.' });
+    } else if (element.name == "rate" && element.validity.rangeUnderflow) {
+        tooltip.setContent({ '.tooltip-inner': 'Doit être positif' });
+    } else if (element.name == "date" && element.validity.rangeUnderflow) {
+        tooltip.setContent({ '.tooltip-inner': 'Doit être égale ou supérieure à aujourd’hui' });
+    } else {
+        console.log("On n'est pas dans la condition");
+    }
+}
+
+//Change fields behaviour when we input the values in fields
+function onChange(element, elHelpText, tooltip) {
+    element.addEventListener("change", (event) => {
+        event.preventDefault();
+        if (element.validity.valid) {
+            tooltip.disable();
+            element.classList.remove("is-invalid");
+            element.classList.add("is-valid");
+            elHelpText.classList.remove("text-danger");
+            elHelpText.classList.add("text-success");
+        } else {
+            element.classList.add("is-invalid");
+            elHelpText.classList.add("text-danger");
+            tooltipMessage(element);
+            tooltip.enable();
+            console.log("pas valide");
+        }
+    });
+
+}
 
 
 // Reset of page after submit
@@ -43,46 +89,10 @@ function submit(form) {
     });
 }
 
-// Create tooltip of error validation
-function tooltip(element) {
-    element.setAttribute("data-bs-toggle", "tooltip");
-    const opt = {
-        trigger: "focus"
-    };
-    const tooltip = bootstrap.Tooltip.getOrCreateInstance(element, opt);
-    tooltip.show();
-}
-
-// Add message to tooltip "error validation"
-function tooltipMessage(element) {
-    if (element.validity.valueMissing) {
-        element.setAttribute("data-bs-title", "Ce champ est obligatoire.");
-    } else if (element.name == "date" && element.validity.rangeUnderflow) {
-        element.setAttribute("data-bs-title", "Doit être égale ou supérieure à aujourd’hui");
-    } else if (element.name == "rate" && element.validity.rangeUnderflow) {
-        element.setAttribute("data-bs-title", "Doit être positif");
-    } else {
-        console.log("On n'est pas dans la condition");
-    }
-}
 
 // Create toast of creating event success 
 function toast() {
     const toastEl = document.getElementById('toast');
     const toast = new bootstrap.Toast(toastEl);
     toast.show();
-}
-
-// Change color of success validation
-function onChangeSuccess(element, elHelpText) {
-    element.addEventListener("change", (event) => {
-        if (element.validity.valid) {
-            event.preventDefault();
-            element.classList.remove("is-invalid");
-            element.classList.add("is-valid");
-            elHelpText.classList.remove("text-danger");
-            elHelpText.classList.add("text-success");
-        }
-    });
-
 }
