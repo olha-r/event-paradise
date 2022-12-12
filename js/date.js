dc964984bd99ce95a53f991e2cf567248a58888e
Raulet Olha <raulet.olha@gmail.com>
2022-12-12 09:33:47 +0100

    Change min value for Rate, change method of  minDate get

--- js/date.js
+++ js/date.js
@@ -1,16 +1,2 @@
 const minDate = document.getElementById("date");
-const currentDate = new Date();
-const day = currentDate.getDate();
-
-if (day < 10) {
-    day = `0${day}`;
-}
-const month = currentDate.getMonth();
-
-if (month < 10) {
-    month = `0${month}`;
-}
-const year = currentDate.getFullYear();
-const today = `${year}-${month + 1}-${day}`;
-
-minDate.setAttribute("min", today);
+minDate.min = new Date().toISOString().split("T")[0];
